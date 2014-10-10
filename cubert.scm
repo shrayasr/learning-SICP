5fb76fb81e490747fe99957a2df59878a4be5ae2
Shrayas <shrayasr@gmail.com>
2014-10-11 00:10:03 +0530

    Finishing cuberoot

--- cubert.scm
+++ cubert.scm
@@ -7,13 +7,13 @@
   (define (square a)
     (* a a))
   (define (improve guess)
-    (/ (+ (/ x (square guess)) (* 2 guess)) 3))
+    (/ (+ (/ x (square guess)) (* 2 guess)) 3.0))
   (define (is-good-enough? guess)
-    (< (abs (- (cube x) guess)) 0.001))
+    (< (abs (- (cube guess) x)) 0.001))
   (define (try guess)
     (if (is-good-enough? guess) 
 	guess
 	(try (improve guess))))
   (try 1))
 
-(cubert 9)
+(cubert 64)
